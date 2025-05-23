5c2960088bf9a980d9cf2e3f9bb737578f906eff
Mauragkas <stathpan03@gmail.com>
2025-05-23 16:29:10 +0300

    cartview text 8===D

--- LendIt/app/src/main/java/com/example/lendit/ui/cart/CartViewModel.kt
+++ LendIt/app/src/main/java/com/example/lendit/ui/cart/CartViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class CartViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is favorites Fragment"
+        value = "This is Cart Fragment"
     }
     val text: LiveData<String> = _text
 }
