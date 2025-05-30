a153cb4bc1bcc28020a4634367d13371e1eba819
Pantelis Flouris <68327978+paflou@users.noreply.github.com>
2025-05-30 18:23:49 +0300

    Update User.kt

--- v1.0/code/app/src/main/java/com/example/lendit/data/local/entities/User.kt
+++ v1.0/code/app/src/main/java/com/example/lendit/data/local/entities/User.kt
@@ -15,57 +15,9 @@ data class UserEntity(
 
     // Owner fields
     var premiumStatus: Boolean? = null,
-    var premiumPlan: String? = null,
+    var premiumPlan: String? = null, // depreciated
     var ratings: Float? = null,
 
     // Coordinator field
     val staffId: String? = null,
 )
-
-class UserClass(
-    val userId: Int,
-    var name: String,
-    val email: String,
-    var password: String,
-    var phoneNumber: String,
-    var location: String,
-    val userType: String,  // "Renter", "Owner", or "Coordinator"
-
-    // Owner fields
-    var premiumStatus: Boolean? = null,
-    var premiumPlan: String? = null,
-    var ratings: Float? = null,
-
-    // Coordinator field
-    val staffId: String? = null,
-) {
-    fun UserEntity.toDomainModel() = UserClass(
-        userId = userId,
-        name = name,
-        email = email,
-        password = password,
-        phoneNumber = phoneNumber,
-        location = location,
-        userType = userType,
-        premiumStatus = premiumStatus,
-        premiumPlan = premiumPlan,
-        ratings = ratings,
-        staffId = staffId
-    )
-
-    fun UserClass.toEntity() = UserEntity(
-        userId = userId,
-        name = name,
-        email = email,
-        password = password,
-        phoneNumber = phoneNumber,
-        location = location,
-        userType = userType,
-        premiumStatus = premiumStatus,
-        premiumPlan = premiumPlan,
-        ratings = ratings,
-        staffId = staffId
-    )
-
-    // You can add methods here if needed, e.g. for updating info, validation, etc.
-}
