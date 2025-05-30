03f732e9b025f25a14650032108d77636da9d7fc
Pantelis Flouris <68327978+paflou@users.noreply.github.com>
2025-05-30 17:14:05 +0300

    merge

--- LendIt/app/src/main/java/com/example/lendit/data/local/managers/FavoritesManager.kt
+++ LendIt/app/src/main/java/com/example/lendit/data/local/managers/FavoritesManager.kt
@@ -2,6 +2,7 @@ package com.example.lendit.data.local.managers
 
 import EquipmentListing
 import android.content.Context
+import com.example.lendit.data.local.entities.Favorite
 import com.example.lendit.data.repository.RepositoryProvider
 
 class FavoritesManager(private val context: Context) {
@@ -34,4 +35,21 @@ class FavoritesManager(private val context: Context) {
 
     val hasFavorites   get() = available.isNotEmpty() || unavailable.isNotEmpty()
     val hasSimilar     get() = similar.isNotEmpty()
+
+    suspend fun isFavorite(userId: Int, listingId: Int): Boolean {
+        return favoriteRepository.isFavorite(userId, listingId)
+    }
+
+    suspend fun addToFavorites(userId: Int, listingId: Int) {
+        val favorite = Favorite(userId = userId, listingId = listingId)
+        favoriteRepository.addToFavorites(favorite)
+    }
+
+    suspend fun removeFromFavorites(userId: Int, listingId: Int) {
+        favoriteRepository.removeFromFavorites(userId, listingId)
+    }
+
+    suspend fun getFavorites(userId: Int): List<Favorite> {
+        return favoriteRepository.getFavorites(userId)
+    }
 }
